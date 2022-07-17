67776f396ddc82777aebbf184ba51ab63b7f2126
subhamLaptop <subhambeura@linlap.com>
2022-07-17 14:17:42 +0530

    added next and prev btn

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,19 +1,23 @@
 import React from 'react'
 import { questionType } from '../questionType'
 import Options from './Options'
+
+import {GrCaretNext,GrCaretPrevious} from "react-icons/gr"
+
 type propsType = {
   question: questionType
 }
 export default function Card({ question }: propsType) {
   console.log(question)
-  const { category, type, difficulty, question: question_text, correct_answer, incorrect_answers } = question
+  const { question: question_text, correct_answer, incorrect_answers } = question
   let allOptions = [...incorrect_answers, correct_answer]
   return (
     <div className='card'>
       <h3>Question 1/10</h3>
       <p>{question_text}</p>
       <Options allOptions={allOptions} />
-
+      <GrCaretPrevious/>
+      <GrCaretNext/>
     </div>
   )
 }
